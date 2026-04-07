aed912cefd46d10b7989dc451ba37955e2154eda
Andrew417 <andrewehab417@gmail.com>
2026-04-08 01:57:52 +0200

    task_7 code finished

---
+++ src/Task 7/Task_7_DP.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int totalStates = 1 << n;
+    vector<int> t(n), p(n), d(n);
+    vector<int> dp(totalStates, INT_MAX);
+    vector<int> last_state(totalStates, -1);
+    vector<int> last_Job(totalStates, -1);
+    vector<int> timeUsed(totalStates, 0);
+
+    for (int i = 0; i < n; i++)
+        cin >> t[i] >> p[i] >> d[i];
+
+    dp[0] = 0;
+
+    for (int mask = 0; mask < totalStates; mask++)
+    {
+        if (dp[mask] == INT_MAX)
+            continue;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (mask & (1 << i))
+                continue;
+
+            int newMask = mask | (1 << i);
+            int newTime = timeUsed[mask] + t[i];
+            int newPenalty = dp[mask];
+
+            if (newTime > d[i])
+                newPenalty += p[i];
+
+            if (newPenalty < dp[newMask])
+            {
+                dp[newMask] = newPenalty;    
+                last_state[newMask] = mask;      
+                last_Job[newMask] = i;       
+                timeUsed[newMask] = newTime; 
+            }
+        }
+    }
+
+    int allDone = totalStates - 1; 
+    vector<int> result;
+    int current = allDone;
+
+    while (current != 0)
+    {
+        result.push_back(last_Job[current]); 
+        current = last_state[current];          
+    }
+    reverse(result.begin(), result.end());
+
+    int currentTime = 0, minPenalties = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        currentTime += t[result[i]];
+        if (currentTime > d[result[i]])
+            minPenalties += p[result[i]];
+
+        cout << i + 1 << ". "
+             << "(job number: " << result[i] + 1 << "), "
+             << "time: " << t[result[i]] << " "
+             << "penalty: " << p[result[i]] << "  "
+             << "deadline: " << d[result[i]] << endl;
+    }
+    cout << "Minimum penalties could occur: " << minPenalties << endl;
+
+    return 0;
+}
---
+++ src/Task 7/Task_7_brute_force.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> t(n), p(n), d(n), result(n), job(n);
+    int time = 0, total_penalties, min_penalties = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> t[i] >> p[i] >> d[i];
+        job[i] = i;
+    }
+
+    do
+    {
+        total_penalties = 0;
+        time = 0;
+        for (int i = 0; i < n; i++)
+        {
+            time += t[job[i]];
+            if (time > d[job[i]])
+                total_penalties += p[job[i]];
+        }
+
+        if (total_penalties < min_penalties)
+        {
+            min_penalties = total_penalties;
+            for (int i = 0; i < n; i++)
+                result[i] = job[i];
+        }
+    } while (next_permutation(job.begin(), job.end()));
+
+    for (int i = 0; i < n; i++)
+        cout << i + 1 << ". " << "(job number: " << result[i] + 1 << "), time: " << t[result[i]] << " penalty: " << p[result[i]] << "  deadline: " << d[result[i]] << endl;
+    cout << "Minimum penalties could occur: " << min_penalties << endl;
+
+    return 0;
+}
